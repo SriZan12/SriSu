07b704858ab69422da22454fc67fb8c3dd7f10b5
SrizanKhadka <khadkasrijan785@gmail.com>
2025-06-17 17:28:37 +0545

    added icons for zodiac sign.

--- composeApp/src/androidMain/kotlin/com/srisu/srisu/previews/ButtonPreviews.kt
+++ composeApp/src/androidMain/kotlin/com/srisu/srisu/previews/ButtonPreviews.kt
@@ -1,14 +1,12 @@
 package com.srisu.srisu.previews
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.srisu.srisu.components.PrimaryButtonCompo
-import com.srisu.srisu.theme.AppTheme
+import com.srisu.srisu.features.auth.screens.GlowingRedIcon
 
 @Composable
 @Preview(showBackground = true)
 fun PreviewPrimaryButton() {
+    GlowingRedIcon()
 }
 
