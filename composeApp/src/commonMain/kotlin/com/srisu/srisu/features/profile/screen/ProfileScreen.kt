a881ee9918a08bbb92a401bc31cc9a05159d9959
SrizanKhadka <khadkasrijan785@gmail.com>
2025-06-20 17:31:49 +0545

    Feat: Update profile image display in Suggestion and Profile Screens
    This commit updates the `AsyncImage` component in both `SuggestionScreen` and `ProfileScreen` to use dynamic profile image URLs instead of static placeholders.
    
    - **SuggestionScreen:**
        - The `AsyncImage` in `SuggestionCard` now loads the image from `suggestionItem?.profilePhoto`.
    - **ProfileScreen:**
        - The `AsyncImage` in `ProfileImageSection` now loads the image from the `profileUrl` variable.
    - Removed debug log statements related to request status in `ProfileScreen`.

--- composeApp/src/commonMain/kotlin/com/srisu/srisu/features/profile/screen/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/srisu/srisu/features/profile/screen/ProfileScreen.kt
@@ -249,7 +249,8 @@ fun ProfilePictureCompo(
             )
         } else {
             AsyncImage(
-                model = "https://images.unsplash.com/photo-1576828831022-ca41d3905fb7?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                model = profileUrl,
+//                model = "https://images.unsplash.com/photo-1576828831022-ca41d3905fb7?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 contentDescription = "Profile Picture",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
@@ -257,9 +258,6 @@ fun ProfilePictureCompo(
                     .height(300.dp)
             )
         }
-
-        AppLogger.log("HAS SENT REQUEST = $hasSentRequest")
-        AppLogger.log("HAS SENT REQUEST = $isRequestSentSuccessfully")
 
         if (hasSentRequest == false && !isRequestSentSuccessfully!!) {
 
