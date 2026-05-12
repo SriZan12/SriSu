fedac770e380d52332276366319d9b3e588f41a8
Srizan12 <srijankhadka89@gmail.com>
2026-05-12 09:45:32 +0545

    refact: zodiac sign icons and background done.

--- composeApp/src/commonMain/kotlin/com/srisu/srisu/features/auth/presentation/screen/authscreen/AddPhoneNumberScreen.kt
+++ composeApp/src/commonMain/kotlin/com/srisu/srisu/features/auth/presentation/screen/authscreen/AddPhoneNumberScreen.kt
@@ -1,5 +1,7 @@
 package com.srisu.srisu.features.auth.presentation.screen.authscreen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -32,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -53,6 +56,7 @@ import com.srisu.srisu.features.auth.presentation.components.ScreenTopIcon
 import com.srisu.srisu.features.auth.presentation.state.AuthUIStates
 import com.srisu.srisu.features.auth.presentation.vm.AuthViewModel
 import com.srisu.srisu.utils.isInternetAvailable
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun PhoneNumberScreen(
@@ -193,6 +197,7 @@ private fun PhoneNumberScreenContent(
     modifier: Modifier = Modifier
 ) {
     val isButtonEnabled = phoneNumber.length >= 10
+    val localFocusManager = LocalFocusManager.current
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
@@ -210,6 +215,12 @@ private fun PhoneNumberScreenContent(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .clickable(
+                    indication = null,
+                    interactionSource = MutableInteractionSource(),
+                    onClick = {
+                        localFocusManager.clearFocus()
+                    })
                 .statusBarsPadding()
                 .verticalScroll(rememberScrollState())
                 .padding(horizontal = 32.dp),
